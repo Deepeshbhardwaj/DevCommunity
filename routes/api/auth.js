e39d57c4dcb91a17bb0aae4603f5c5d642c06c1d
DeepeshBhardwaj <dipeshbhardwaj95@gmail.com>
2020-06-07 15:19:14 +0530

    Created Authenticated User

---
+++ routes/api/auth.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config')
+const { check, validationResult } = require('express-validator');
+
+
+// @route    GET api/auth
+// @desc     Test route
+// @access   Public
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user)
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Sevrer Error')
+    }
+});
+
+module.exports = router;
+
+
+
+// @route    GET api/users
+// @desc     Register route
+// @access   Public
+router.post('/',[
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+        'password',
+        'Password is Required'
+    ).exists()],
+    async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password} = req.body
+
+    try {
+
+        // See if the user exists
+
+        let user = await User.findOne({email});
+
+        if(!user) {
+            return res.status(400).json({errors: [{msg: 'Invalid Credentials'}]});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            return res.status(400).json({errors: [{msg: 'Invalid Credentials'}]});
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        
+        jwt.sign(payload,config.get('jwtSecret'),{expiresIn: 3600000}, (err,token)=> {
+            if(err) throw err;  
+            res.json({token})
+        });
+
+        
+    } catch (error) {
+        console.log(err.message);
+        res.status(500).send('Server error')
+    }
+
+
+    
+});
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+connectDB();
+
+// Init MiddleWare
+app.use(express.json({extended : false}))
+
 app.get('/', (req,res) => res.send('API Running'))
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 
 const PORT = process.env.PORT || 5000
 
