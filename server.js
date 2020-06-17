c1e8b967b0cd95589e7731867bedc1fdfa925c8e
DeepeshBhardwaj <dipeshbhardwaj95@gmail.com>
2020-06-17 10:40:22 +0530

    Deployment

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 connectDB();
 
 // Init MiddleWare
-app.use(express.json({extended : false}))
-
-app.get('/', (req,res) => res.send('API Running'))
+app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -16,7 +15,15 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Server Static assests in production
+if (process.env.NODE_ENV === 'production') {
+  // set Static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
